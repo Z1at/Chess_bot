a95b7c7854b7d5f5968627b37f5e3f52d1d4cf79
1at <nikandrov2103@gmail.com>
2025-05-29 22:27:16 +0300

    added second file with openings2

---
+++ lichess-bot/engines/bot/tmp.py
@@ -0,0 +1,49 @@
+import os
+import pandas as pd
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+# file_path = os.path.join(current_directory, 'high_elo_opening.csv')
+
+    # Get all of the SAN notations
+# chess_openings = pd.read_csv(file_path, on_bad_lines='skip')
+
+# print(chess_openings)
+# for i in chess_openings['moves_list']:
+#     print(i)
+
+# with (open(r"C:\Users\nikan\Desktop\Chess_bot\lichess-bot\engines\bot\high_elo_opening.txt", "r") as f,
+#       open(r"C:\Users\nikan\Desktop\Chess_bot\lichess-bot\engines\bot\high_elo_opening_result.txt", "w") as g):
+#     for i in f.readlines()[1:]:
+#         s = i.strip()[1:-1]
+#         s = s.split('\',')
+#
+#         # print(s)
+#         for k in range(1, 20):
+#             for j in range(len(s)):
+#                 # print(s[j])
+#                 if len(s[j].split('\'' + str(k) + ".")) > 1:
+#                     s[j] = s[j].split('\'' + str(k) + ".")[1]
+#                 # print(s[j])
+#
+#         for j in range(len(s)):
+#             for k in s[j].split('\''):
+#                 if k != "":
+#                     s[j] = k
+#         # print(s)
+#
+#
+#         g.write(" ".join(s) + '\n')
+
+
+# with open(r"C:\Users\nikan\Desktop\Chess_bot\lichess-bot\engines\bot\high_elo_opening_result.txt", "w") as g:
+#     g.write("Jopa")
+
+
+def ad(a):
+    a.append(2)
+    return a
+
+b = [1]
+ad(b)
+
+print(b)
